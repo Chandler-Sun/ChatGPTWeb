100b4707f19e0bbf5325d201a56d2d5450dca095
Chandler Sun <tlwanan@gmail.com>
2023-03-29 00:09:42 +0800

    change avatar

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://assets.belloai.com/staging/config/menu_logo.png',
+      name: '内部测试请勿外发',
+      description: '<a href="https://newzone.top/chatgpt/" class="text-blue-500" target="_blank" >使用参考</a>',
     },
   }
 }
